149352c5457978a9c6c4df9fe5781c6d7d530341
Matt Burdan <burdz@burdz.net>
2018-06-23 19:16:26 -0700

    use curl

--- travisInstall.sh
+++ travisInstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export DEP_VERSION=0.4.1
-wget https://github.com/golang/dep/releases/download/v${DEP_VERSION}/dep-linux-amd64 && mv dep-linux-amd64 dep && chmod +x dep
-
+curl -L -s https://github.com/golang/dep/releases/download/v${DEP_VERSION}/dep-linux-amd64 -o dep
+chmod +x dep
 ./dep ensure
