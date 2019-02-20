a71b932990f04f1202b98d85c0d07d73ff20bd32
Jacob Kiefer <jacobkiefer@google.com>
2019-02-20 10:44:07 -0500

    feat(pipeline/execution): Adds get cmd for pipeline executions.

---
+++ cmd/pipeline/execution/get.go
@@ -0,0 +1,80 @@
+// Copyright (c) 2019, Google, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+package execution
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/spf13/cobra"
+	"github.com/spinnaker/spin/cmd/gateclient"
+	"github.com/spinnaker/spin/util"
+)
+
+type GetOptions struct {
+	*executionOptions
+	output      string
+	application string
+	name        string
+}
+
+var (
+	getExecutionShort = "Get the specified execution"
+	getExecutionLong  = "Get the execution with the provided id "
+)
+
+func NewGetCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "get",
+		Short: getExecutionShort,
+		Long:  getExecutionLong,
+		RunE:  getExecution,
+	}
+	return cmd
+}
+
+func getExecution(cmd *cobra.Command, args []string) error {
+	gateClient, err := gateclient.NewGateClient(cmd.InheritedFlags())
+	if err != nil {
+		return err
+	}
+
+	if len(args) == 0 || args[0] == "" {
+		return errors.New("pipeline execution id required")
+	}
+	id := args[0]
+
+	query := map[string]interface{}{
+		"executionIds": id, // Status filtering is ignored when executionId is supplied
+		"limit":        int32(1),
+	}
+
+	successPayload, resp, err := gateClient.ExecutionsControllerApi.GetLatestExecutionsByConfigIdsUsingGET(
+		gateClient.Context, query)
+
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Encountered an error getting execution %s, status code: %d\n",
+			id,
+			resp.StatusCode)
+	}
+
+	util.UI.JsonOutput(successPayload, util.UI.OutputFormat)
+	return nil
+}
