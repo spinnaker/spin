f0538f83848434afcebbd2c32e8515e39056c250
Michael Graff <explorer@flame.org>
2023-03-31 15:00:31 -0500

    fix: fix command/config timeout parse

--- cmd/root.go
+++ cmd/root.go
@@ -44,7 +44,7 @@ func NewCmdRoot(outWriter, errWriter io.Writer) (*cobra.Command, *RootOptions) {
 	cmd.PersistentFlags().BoolVarP(&options.ignoreCertErrors, "insecure", "k", false, "ignore certificate errors")
 	cmd.PersistentFlags().BoolVarP(&options.ignoreRedirects, "ignore-redirects", "", false, "ignore redirects")
 	cmd.PersistentFlags().StringVar(&options.defaultHeaders, "default-headers", "", "configure default headers for gate client as comma separated list (e.g. key1=value1,key2=value2)")
-	cmd.PersistentFlags().IntVar(&options.retryTimeout, "retry-timeout", 60, "maximum time to wait for tasks to complete in seconds (default 60)")
+	cmd.PersistentFlags().IntVar(&options.retryTimeout, "retry-timeout", 0, "maximum time to wait for tasks to complete in seconds (default 60)")
 
 	// UI Flags
 	cmd.PersistentFlags().BoolVarP(&options.quiet, "quiet", "q", false, "squelch non-essential output")
